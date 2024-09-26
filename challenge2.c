a7f89191a4ee2ae4d84c3ef8d6c8d514574d9caa
salma hamdi <saalmahm@gmail.com>
2024-09-26 15:59:35 +0100

    challenge 2

--- challenge2.c
+++ challenge2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-int n;
-int T[50],val,i ;
+	int n,i;
+float T[50],val ;
     printf("Entrer le nombre des élément de tableau: \n");
     scanf("%d",&n);
     for ( i=0;i<n;i++){
         printf("Entrer la valeur %d \n",i+1);
-        scanf("%d",&T[i]);
+        scanf("%f",&T[i]);
 }
 printf("voila les éléments du tableaux: \n");
 for(i=0;i<n;i++){
-    printf("%d \n",T[i]);
+    printf("%.2f \n",T[i]);
 }
 
     return 0;
